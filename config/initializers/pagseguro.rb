10502dfb6f385db73632a900a9584cd28a233ce2
gzbordignon <gzbordignon@gmail.com>
2020-07-29 13:47:47 -0300

    commit

--- config/initializers/pagseguro.rb
+++ config/initializers/pagseguro.rb
@@ -1,6 +1,6 @@
 PagSeguro.configure do |config|
   config.token       = ENV["PAGSEGURO_TOKEN"]
   config.email       = ENV["PAGSEGURO_EMAIL"]
-  config.environment = ENV["PAGSEGURO_ENVIRONMENT"]
+  config.environment = ENV["PAGSEGURO_ENVIRONMENT"].to_sym
   config.encoding    = ENV["PAGSEGURO_ENCODING"]
 end
