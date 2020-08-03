430339f66193ef4084b34c9e74d949e12a9f284c
gzbordignon <gzbordignon@gmail.com>
2020-08-03 20:15:33 -0300

    commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,21 +89,22 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => 'gmail.com' }
-
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-      :address              => 'smtp.gmail.com',
-      :port                 => 587,
-      :domain               => 'gmail.com',
-      :user_name            => 'gzbordignon@gmail.com',
-      :password             => '26628426',
+  config.action_mailer.default_url_options = { :host => 'https://telegsul.herokuapp.com/' }
+  
+  ActionMailer::Base.smtp_settings = {
+      :address              => 'smtp.sendgrid.net',
+      :port                 => '587',
+      :domain               => 'heroku.com',
+      :user_name            => ENV['SENDGRID_USERNAME'],
+      :password             => ENV['SENDGRID_PASSWORD'],
       :authentication       => 'plain',
       :enable_starttls_auto => true
   }
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.perform_caching = false
+
+
 
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
