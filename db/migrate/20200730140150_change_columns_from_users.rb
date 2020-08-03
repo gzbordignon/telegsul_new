1e7db3b840bf1127f692de65773b39f69b296f6e
gzbordignon <gzbordignon@gmail.com>
2020-08-03 19:53:49 -0300

    commit

--- db/migrate/20200730140150_change_columns_from_users.rb
+++ db/migrate/20200730140150_change_columns_from_users.rb
@@ -1,6 +1,6 @@
 class ChangeColumnsFromUsers < ActiveRecord::Migration[6.0]
   def change
-  	change_column :users, :document_type, :integer
+  	change_column :users, :document_type, 'integer USING CAST(document_type AS integer)'
   	change_column :users, :document_number, :string
   end
 end
